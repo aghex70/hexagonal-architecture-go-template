e5be3cff0ae3c9c795428ab877660ef761fe998f
aghex70 <aghex70@gmail.com>
2023-02-22 02:22:23 +0100

    Fixed RESTful urls

--- templates/rest.go
+++ templates/rest.go
@@ -29,10 +29,10 @@ func (s *RestServer) StartServer() error {
 `
 
 const StartServerHandlersTemplate = `	// {{.TableSuffix}}
-	router.POST("/{{.LowerEntity}}", s.{{.LowerEntity}}Handler.Create{{.Entity}})
-	router.GET("/{{.LowerEntity}}/:uuid", s.{{.LowerEntity}}Handler.Get{{.Entity}})
-	router.PUT("/{{.LowerEntity}}/:uuid", s.{{.LowerEntity}}Handler.Update{{.Entity}})
-	router.DELETE("/{{.LowerEntity}}/:uuid", s.{{.LowerEntity}}Handler.Delete{{.Entity}})
+	router.POST("/{{.TableSuffix}}", s.{{.LowerEntity}}Handler.Create{{.Entity}})
+	router.GET("/{{.TableSuffix}}/:uuid", s.{{.LowerEntity}}Handler.Get{{.Entity}})
+	router.PUT("/{{.TableSuffix}}/:uuid", s.{{.LowerEntity}}Handler.Update{{.Entity}})
+	router.DELETE("/{{.TableSuffix}}/:uuid", s.{{.LowerEntity}}Handler.Delete{{.Entity}})
 	router.GET("/{{.TableSuffix}}", s.{{.LowerEntity}}Handler.List)
 
 `
